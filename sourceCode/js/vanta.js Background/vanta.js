1266bca38724a251b36a05c6b659b5d5fb7b0afe
kevin23566 <adiwigunakevin@gmail.com>
2022-03-06 12:38:49 +0800

    First commit

---
+++ sourceCode/js/vanta.js Background/vanta.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#content",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffffff,
+  backgroundColor: 0x0,
+  points: 12.00,
+  maxDistance: 15.00,
+  spacing: 17.00
+})
